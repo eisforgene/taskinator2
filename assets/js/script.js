8a5319b8c28359b47d7ca8e05b59d785940ef3a8
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-02-11 17:28:51 -0800

    address usability concerns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,37 @@
 let formEl = document.querySelector('#task-form');
 let tasksToDoEl = document.querySelector('#tasks-to-do');
 
-let createTaskHandler = (event) => {
+let taskFormHandler = (event) => {
     event.preventDefault();
 
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    
+    createTaskEl(taskDataObj);
+};
+
+let createTaskEl = (taskDataObj) => {
     let listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
 
     let taskInfoEl = document.createElement('div');
     taskInfoEl.className = 'task-info';
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     listItemEl.appendChild(taskInfoEl);
-    
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
-formEl.addEventListener('submit', createTaskHandler);
+formEl.addEventListener('submit', taskFormHandler);
